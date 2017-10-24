c4abd193d13bf50e012563cce3ec1c27228bd5dd
jehung <jehung@me.com>
2017-10-24 01:32:19 -0700

    Add visualization function for clusters

--- clustering.py
+++ clustering.py
@@ -14,6 +14,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV, StratifiedShuffleSplit
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture as GM
+from sklearn.decomposition import PCA
 from collections import defaultdict
 from helpers import cluster_acc, myGMM,nn_arch,nn_reg
 import sklearn.metrics as metrics
@@ -21,58 +22,33 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 import sys
 import utility
+import pylab as pl
+from scipy.spatial.distance import cdist
 
 
-
-
-
-'''
-# %% For chart 4/5
-madelonX2D = TSNE(verbose=10,random_state=5).fit_transform(madelonX)
-digitsX2D = TSNE(verbose=10,random_state=5).fit_transform(digitsX)
-
-madelon2D = pd.DataFrame(np.hstack((madelonX2D,np.atleast_2d(madelonY).T)),columns=['x','y','target'])
-digits2D = pd.DataFrame(np.hstack((digitsX2D,np.atleast_2d(digitsY).T)),columns=['x','y','target'])
-
-madelon2D.to_csv(out+'madelon2D.csv')
-digits2D.to_csv(out+'digits2D.csv')
-'''
-
-
-'''
-def bench_k_means(estimator, name, data, labels):
-    t0 = clock()
-    estimator.fit(data)
-    print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
-          % (name, (clock() - t0), estimator.inertia_,
-             metrics.homogeneity_score(labels, estimator.labels_),
-             metrics.completeness_score(labels, estimator.labels_),
-             metrics.v_measure_score(labels, estimator.labels_),
-             metrics.adjusted_rand_score(labels, estimator.labels_),
-             metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
-             metrics.silhouette_score(data, estimator.labels_,
-                                      metric='euclidean',
-                                      sample_size=sample_size)))
-'''
 
 
 
 def evaluate_kmeans(X, y, problem):
     SSE = defaultdict(dict)
     ll = defaultdict(dict)
+    distort_km = []
+    distort_gm = []
     acc = defaultdict(lambda: defaultdict(dict))
     adjMI = defaultdict(lambda: defaultdict(dict))
     km = KMeans(random_state=5)
     gm = GM(random_state=5)
 
     st = clock()
-    clusters = [2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20]
+    clusters = [2, 3, 4, 5, 6]
     for k in clusters:
         print('now doing k=' + str(k))
         km.set_params(n_clusters=k)
         gm.set_params(n_components=k)
         km.fit(X)
         gm.fit(X)
+        distort_km.append(sum(np.min(cdist(X, km.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
+        #distort_gm.append(sum(np.min(cdist(X, gm.cluster_centers_, 'euclidean'), axis=1)) / X.shape[0])
         SSE[k][problem] = km.score(X)
         ll[k][problem] = gm.score(X)
         print('km score:', SSE[k][problem])
@@ -98,11 +74,46 @@ def evaluate_kmeans(X, y, problem):
     adjMI.ix[:,:,problem] .to_csv(problem+' adjMI.csv')
     adjMI.ix[:,:,problem].to_csv(problem + ' adjMI.csv')
 
-    return SSE, ll, acc, adjMI
+    initial = [cluster.vq.kmeans(tests, k) for k in clusters]
+    pyplot.plot([var for (cent, var) in initial])
+    pyplot.show()
+
+    return SSE, ll, acc, adjMI, km, gm
 
 
+def visualize_clusters(data, target, problem):
+    pca = PCA(n_components=2).fit(data)
+    pca_2d = pca.transform(data)
 
-def clustering(X, y):
+    '''
+    # visualize actual class in new projected space
+    for i in range(0, pca_2d.shape[0]):
+        if target[i] == 0:
+            c1 = pl.scatter(pca_2d[i, 0], pca_2d[i, 1], c='r',
+                         marker='+')
+        elif target[i] == 1:
+            c2 = pl.scatter(pca_2d[i, 0], pca_2d[i, 1], c='g',
+                         marker='o')
+        elif target[i] == 2:
+            c3 = pl.scatter(pca_2d[i, 0], pca_2d[i, 1], c='b',
+                         marker='*')
+    #pl.legend([c1, c2, c3], ['Setosa', 'Versicolor',
+    #                              'Virginica'])
+    pl.title('Insert title here')
+    pl.show()
+    '''
+    # now visualize classified data in new projected space
+    pl.figure('Reference Plot ' + problem)
+    pl.scatter(pca_2d[:, 0], pca_2d[:, 1], c=['g', 'r'], label=target)
+    kmeans = KMeans(n_clusters=2)
+    kmeans.fit(data)
+    pl.figure('K-means with 2 clusters ' + problem)
+    pl.scatter(pca_2d[:, 0], pca_2d[:, 1], c=['g', 'r'], label=kmeans.labels_)
+    pl.legend()
+    pl.show()
+
+
+def clustering_nn(X, y):
     mlp = MLPClassifier(solver='adam', alpha=1e-5, shuffle=True, early_stopping=True, activation='relu',
                         verbose=True)
     parameters = {
@@ -128,18 +139,21 @@ def clustering(X, y):
     return clf, gs.best_score_, gs
 
 
+
 if __name__ == '__main__':
     all_data = utility.get_all_data()
     train, target = utility.process_data(all_data)
     SSE, ll, acc, adjMI = evaluate_kmeans(train, target, 'FreddieMac')
-    clf, score, gs = clustering(train, target)
-    tmp = pd.DataFrame(gs.cv_results_)
-    tmp.to_csv('FreddieMac NN.csv')
+    visualize_clusters(train, target, 'FreddieMac')
+    #clf, score, gs = clustering_nn(train, target)
+    #tmp = pd.DataFrame(gs.cv_results_)
+    #tmp.to_csv('FreddieMac NN.csv')
 
 
     all_data = utility.get_all_data_bloodDonation()
     train, target = utility.process_data_bloodDonation(all_data)
     SSE, ll, acc, adjMI = evaluate_kmeans(train, target, 'BloodDonation')
-    clf, score, gs = clustering(train, target)
-    tmp = pd.DataFrame(gs.cv_results_)
-    tmp.to_csv('BloodDonation NN.csv')
+    visualize_clusters(train, target, 'Blood Donation')
+    #clf, score, gs = clustering_nn(train, target)
+    #tmp = pd.DataFrame(gs.cv_results_)
+    #tmp.to_csv('BloodDonation NN.csv')
