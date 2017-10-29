22b6c34639c8ac4da9e8ecb635d3b4917dc84deb
jehung <jehung@me.com>
2017-10-29 15:10:52 -0700

    Correct code for getting eigenvalues

--- PCA.py
+++ PCA.py
@@ -17,16 +17,19 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import roc_auc_score
 from joblib import Parallel, delayed
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 import pylab as pl
 import clustering
 from imblearn.over_sampling import SMOTE
+import collections
 pd.set_option('display.max_columns', None)
+
 
 out = './results/PCA/'
 #cmap = cm.get_cmap('Spectral')
 
-clusters =  [2,5,10,15,20,25,30,35,40]
-dims = [2,5,10,15,20,25,30,35,40,45,50,55,60]
+clusters =  [2,5,10]
+dims = [2,5,10,15,20]
 
 
 
@@ -53,13 +56,21 @@ def visualize_pca(X, y, problem):
     pl.show()
 
 
-
-
 def pca(X, problem):
+    dims = [2,5,10,15,20]
     pca = PCA(random_state=5)
-    pca.fit(X)
-    tmp = pd.Series(data = pca.explained_variance_,index=range(len(X[0])))
-    tmp.to_csv(out+problem+'.csv')
+    if 'Blood' in problem:
+        dims = range(2, len(X[0]))
+    eigen = collections.defaultdict(dict)
+    for dim in dims:
+        pca.set_params(n_components=dim)
+        pca.fit(X)
+        eigen[dim]['explained_var']=np.sum(pca.fit(X).explained_variance_ratio_)
+        eigen[dim]['eigenval'] = pca.explained_variance_
+
+    eigen = pd.DataFrame.from_dict(eigen)
+    eigen.to_csv(out+problem+'PCA.csv')
+
 
 
 def reduction_clustering(X, y, k, problem):
@@ -67,7 +78,7 @@ def reduction_clustering(X, y, k, problem):
     pca = PCA(n_components=k)
     X_pca = pca.fit(X).transform(X)
 
-    return clustering.evaluate_kmeans(X_pca, y, problem)
+    return clustering.evaluate_kmeans(X_pca, y, problem, out='./results/PCA/')
     #TODO: should we perform plotting?
 
 
@@ -109,28 +120,46 @@ def pca_nn(X, y, problem):
     return clf, gs.best_score_, gs
 
 
-'''  ## what is this?
-def visualize_data(dim, X, y, problem):
-    dim = 5
-    pca = PCA(n_components=dim,random_state=10)
 
-    dataX2 = pca.fit_transform(X)
-    data2 = pd.DataFrame(np.hstack((dataX2,np.atleast_2d(y).T)))
-    cols = list(range(data2.shape[1]))
-    cols[-1] = 'Class'
-    data2.columns = cols
-    data2.to_hdf(out+'datasets.hdf',problem,complib='blosc',complevel=9)
 
-    dim = 60
-    pca = PCA(n_components=dim,random_state=10)
-    digitsX2 = pca.fit_transform(digitsX)
-    digits2 = pd.DataFrame(np.hstack((digitsX2,np.atleast_2d(digitsY).T)))
-    cols = list(range(digits2.shape[1]))
-    cols[-1] = 'Class'
-    digits2.columns = cols
-    digits2.to_hdf(out+'datasets.hdf','digits',complib='blosc',complevel=9)
-'''
+def reduction_cluster_nn(X,y,problem):
+    n = len(X[0])
+    sm = SMOTE()
+    pca = PCA(random_state=5)
+    km = KMeans(random_state=5)
+    mlp = MLPClassifier(solver='adam', alpha=1e-5, shuffle=True, early_stopping=True, activation='relu',
+                        verbose=True)
+    X_res, y_res = sm.fit_sample(X, y)
+    if 'Freddie' in problem:
+        pca__n_components = [2, 3, 4, 5, 6, 7]
+    else:
+        pca__n_components = [2, 3]
 
+    parameters = {
+        'NN__hidden_layer_sizes': [(n,), (n, n, n), (n, n, n, n, n),
+                                   (n,), (n, int(0.9 * n), int(0.9 * n)),
+                                   (n, int(0.9 * n), int(0.9 * n), int(0.9 * n), int(0.9 * n)),
+                                   (n,), (n, int(0.8 * n), int(0.8 * n)),
+                                   (n, int(0.8 * n), int(0.8 * n), int(0.8 * n), int(0.8 * n)),
+                                   (n,), (n, int(0.7 * n), int(0.7 * n)),
+                                   (n, int(0.7 * n), int(0.7 * n), int(0.7 * n), int(0.7 * n))],
+        'pca__n_components': pca__n_components,
+        'km__n_clusters': [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    }
+
+    sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2)  ## no need for this given 50000 random sample
+    pipe = Pipeline([('pca', pca), ('km', km), ('NN', mlp)])
+    gs = GridSearchCV(pipe, parameters, verbose=10, cv=sss)
+
+    gs.fit(X_res, y_res)
+    clf = gs.best_estimator_
+    print(clf)
+    print(gs.best_score_)
+
+    tmp = pd.DataFrame(gs.cv_results_)
+    tmp.to_csv(out + problem + ' dim_red_nn.csv')
+
+    return clf, gs.best_score_, gs
 
 
 if __name__ == '__main__':
@@ -139,17 +168,23 @@ if __name__ == '__main__':
     #visualize_pca(train, target, 'FreddieMac')
     #pca(train, 'FreddieMac')
     #reduction_clustering(train, target, 20, 'FreddiMac')
-    clf, score, gs = pca_nn(train, target, 'FreddieMac')
+    #clf, score, gs = pca_nn(train, target, 'FreddieMac')
     #tmp = pd.DataFrame(gs.cv_results_)
     #tmp.to_csv('FreddieMac NN.csv')
     #visualize_data(5, train, target, 'FreddieMac')
+    clf, score, gs = reduction_cluster_nn(train, target, 'FreddieMac')
+    tmp = pd.DataFrame(gs.cv_results_)
+    tmp.to_csv('FreddieMac dr_cluster_NN.csv')
 
     all_data = utility.get_all_data_bloodDonation()
     train, target = utility.process_data_bloodDonation(all_data)
-    #visualize_pca(train, target, 'BloodDonation')
+    visualize_pca(train, target, 'BloodDonation')
     #pca(train, 'BloodDonation')
     #reduction_clustering(train, target, 2, 'BloodDonation')
-    clf, score, gs = pca_nn(train, target, 'BloodDonation')
+    #clf, score, gs = pca_nn(train, target, 'BloodDonation')
     #tmp = pd.DataFrame(gs.cv_results_)
     #tmp.to_csv('BloodDonation NN.csv')
     #visualize_data(5, train, target, 'BloodDonation')
+    clf, score, gs = reduction_cluster_nn(train, target, 'FreddieMac')
+    tmp = pd.DataFrame(gs.cv_results_)
+    tmp.to_csv('FreddieMac dr_cluster_NN.csv')
